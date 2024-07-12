e651cdb05a7ab7f872f2b2c7fd1e9eb2cd479831
RicarDitouu <20220039@ricaldone.edu.sv>
2024-07-12 08:13:21 -0600

    si

--- firebase-app-Leito/src/config/firebase.js
+++ firebase-app-Leito/src/config/firebase.js
@@ -1,19 +1,16 @@
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDps2R0oEbOoEYrazQD-rr1eBE2atCSrK4",
-  authDomain: "practica-firebase-20220039.firebaseapp.com",
-  projectId: "practica-firebase-20220039",
-  storageBucket: "practica-firebase-20220039.appspot.com",
-  messagingSenderId: "361715627912",
-  appId: "1:361715627912:web:0a6929cb634b274823ac3d"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID    
 };
 
 console.log("Valor de configuracion", firebaseConfig);
