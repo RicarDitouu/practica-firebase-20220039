370f986c79330e7772df9884033cd71385fc07e4
RicarDitouu <20220039@ricaldone.edu.sv>
2024-07-12 07:57:55 -0600

    si

---
+++ firebase-app-Leito/src/config/firebase.js
@@ -0,0 +1,44 @@
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from 'firebase/storage';
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDps2R0oEbOoEYrazQD-rr1eBE2atCSrK4",
+  authDomain: "practica-firebase-20220039.firebaseapp.com",
+  projectId: "practica-firebase-20220039",
+  storageBucket: "practica-firebase-20220039.appspot.com",
+  messagingSenderId: "361715627912",
+  appId: "1:361715627912:web:0a6929cb634b274823ac3d"
+};
+
+console.log("Valor de configuracion", firebaseConfig);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+if (app) {
+  console.log('Firebase initialized successfully');
+} else {
+  console.log('Firebase initialization failed');
+}
+
+const database = getFirestore(app);
+if (database) {
+  console.log('Firestore initialized correctly');
+} else {
+  console.log('Firestore initialization failed');
+}
+
+const storage = getStorage(app);
+
+if (storage) {
+  console.log('storage initialized correctly');
+} else {
+  console.log('storage initialization failed');
+}
+
+export { database,storage };
